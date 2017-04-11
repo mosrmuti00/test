a252510a114da8e6c872ee110897da9c85269226
mosrmuti00 <jaruwat.je@rmuti.ac.th>
2017-04-11 17:37:32 +0700

    test

--- src/main/java/rmuti/ArrayUtil/ArrayUtil.java
+++ src/main/java/rmuti/ArrayUtil/ArrayUtil.java
@@ -1,27 +1,24 @@
 package rmuti.ArrayUtil;
 
-public class ArrayUtil 
-{
-	public String contactArray(String[] strArr)
-	{
-		String str = " ";
-		for(int i = 0; i<4; i++)
-		{
-			str = str+strArr[i];
+public class ArrayUtil {
+	public String contactArray(String[] strArr) {
+		String str = "";
+		for (int i = 0; i < strArr.length; i++) {
+			str = str + strArr[i];
 		}
 		return str;
 	}
-	
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		ArrayUtil arr = new ArrayUtil();
-		String[] str = new String[4];
+		String[] str = new String[5];
 		str[0] = "str0 ";
 		str[1] = "str1 ";
 		str[2] = "str2 ";
 		str[3] = "str3 ";
+		str[4] = "str4 ";
 		
 		System.out.println(arr.contactArray(str));
 	}
-	
+
 }
